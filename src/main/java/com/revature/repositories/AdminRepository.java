f6376f0e79c01922119f1ea517c5656543ae9498
Ebenezer Lewis <htdke12@gmail.com>
2019-01-04 16:11:56 -0500

    setting up h2 database need revised

--- src/main/java/com/revature/repositories/AdminRepository.java
+++ src/main/java/com/revature/repositories/AdminRepository.java
@@ -1,5 +1,28 @@
 package com.revature.repositories;
 
-public interface AdminRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+	
+	
+	public List<Admin> findAll();
+
+	public Admin findByUsername(String username);
+	public Admin findByFirstname(String firstname);
+	
+	
+	@Query(value = "UPDATE admin SET approve = true WHERE id = ?1 RETURNING id", nativeQuery = true)
+	public int setapprove(int id);
+	
+	
+	
+	
 
 }
