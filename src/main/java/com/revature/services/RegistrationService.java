6c775f2d451493264410b2e82c091862caf4531c
Jaron Ewell <jaron@outlook.com>
2019-01-14 11:58:18 -0500

    added hystrix to send verification email

--- src/main/java/com/revature/services/RegistrationService.java
+++ src/main/java/com/revature/services/RegistrationService.java
@@ -1,14 +1,18 @@
 package com.revature.services;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.revature.dtos.AdminDto;
 import com.revature.models.Admin;
+import com.revature.models.VerificationEmail;
 
 @Service
 public class RegistrationService {
@@ -16,8 +20,11 @@ public class RegistrationService {
 	@Autowired
 	AdminService adminService;
 	
-	@Value("${RMS_RESOURCE_URL:http://localhost:8080/email}")
+	@Value("${RMS_RESOURCE_URL:http://localhost:8080/email/}")
 	String emailUri;
+	
+	@Value("${RMS_RESOURCE_URL:http://localhost:8080/admin-registration/}")
+	String registrationUri;
 	
 	public Boolean registerNewAdmin(AdminDto adminDto) {
 
@@ -32,9 +39,34 @@ public class RegistrationService {
 		
 		adminService.createAdmin(admin);
 		
-		emailUri + "/sendadminconfirmation";
+		sendEmail(admin.getUsername(), admin.getVerificationCode());
 		
 		return true;
+		
+	}
+	
+	/**
+	 * 
+	 * Sends an email to the admin with a link to click on to verify their account.
+	 * 
+	 * @param email
+	 * @param verificationCode
+	 */
+	@HystrixCommand(fallbackMethod = "sendEmailFallback")
+	private void sendEmail(String email, String verificationCode) {
+		VerificationEmail verificationEmail = new VerificationEmail(email, registrationUri + "registration?" + verificationCode);
+		new RestTemplate().postForLocation(URI.create(emailUri + "sendadminconfirmation"), verificationEmail);
+	}
+	
+	/**
+	 * 
+	 * This method will be called as a fallback method if the hystrix command to send the email fails.
+	 * This allows the user to be notified if there is a problem sending the email.
+	 * 
+	 * @param email
+	 * @param verificationCode
+	 */
+	private void sendEmailFallback(String email, String verificationCode) {
 		
 	}
 	
